ad1869564d4369fe3f71b464f87107ad17748984
Sistem Yoneticisi <sistem@arsiv.local>
2026-06-19 16:39:48 +0300

    ilk kararli surum

---
+++ github_yukle.bat
@@ -0,0 +1,7 @@
+@echo off
+echo GitHub'a yukleme baslatiliyor...
+echo Lutfen acilan pencereden GitHub'a giris yapin (Sign in with your browser)
+"C:\Program Files\Git\bin\git.exe" push -u origin main
+echo.
+echo Islem tamamlandi.
+pause
